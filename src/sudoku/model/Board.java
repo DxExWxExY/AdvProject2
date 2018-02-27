7b88525204440049ae60caa0be59605279ea948e
DxExWxExY <DxExWxExY@gmail.com>
2018-02-27 13:56:45 -0700

    Created Branch

--- src/sudoku/model/Board.java
+++ src/sudoku/model/Board.java
@@ -5,10 +5,12 @@ public class Board {
 
     /** Size of this board (number of columns/rows). */
     public final int size;
+    public int[][] board;
 
     /** Create a new board of the given size. */
     public Board(int size) {
         this.size = size;
+        this.board = new int[size][size];
 
         // WRITE YOUR CODE HERE ...
     }
